6205f517483faa78e2202160ebf8c44afcdf2e54
Calliixte <alicee.baco@gmail.com>
2025-01-27 19:06:19 +0100

    v if experimentation

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,20 @@ const app = Vue.createApp({
         return{
             title : 'Welcome to my vue training range',
             nb : 0,
+            clickPower : 1,
+            upgradesShown : false,
         };
     },
     methods : {
         increment(){
-            this.nb++;
+            this.nb+=this.clickPower;
             console.log('Test');
+        },
+        showUpgrades(){
+            this.upgradesShown = !this.upgradesShown;
+        },
+        changeClickPower(nb){
+            this.clickPower+=nb;
         }
     }
 
